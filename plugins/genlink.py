ac64d523feb9f5f8b8ad479435a9eb87bd397140
EpicSenpai <arvindrathod5476@gmail.com>
2026-06-12 13:19:57 +0530

    Update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -1,12 +1,10 @@
 import re
-from pyrogram import filters, Client, enums
-from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
-from config import ADMINS, LOG_CHANNEL, PUBLIC_FILE_STORE, WEBSITE_URL, WEBSITE_URL_MODE
-from plugins.users_api import get_user, get_short_link
-import re
 import os
 import json
 import base64
+from pyrogram import filters, Client, enums
+from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, UsernameInvalid, UsernameNotModified
+from config import ADMINS, LOG_CHANNEL, PUBLIC_FILE_STORE, WEBSITE_URL, WEBSITE_URL_MODE
 
 async def allowed(_, __, message):
     if PUBLIC_FILE_STORE:
@@ -18,24 +16,21 @@ async def allowed(_, __, message):
 @Client.on_message((filters.document | filters.video | filters.audio) & filters.private & filters.create(allowed))
 async def incoming_gen_link(bot, message):
     username = (await bot.get_me()).username
-    file_type = message.media
-    post = await message.copy(LOG_CHANNEL)
+    try:
+        post = await message.copy(LOG_CHANNEL)
+    except Exception as e:
+        return await message.reply(f"<b>❌ ᴇʀʀᴏʀ: ʙᴏᴛ ɪs ɴᴏᴛ ᴀᴅᴍɪɴ ɪɴ ʟᴏɢ ᴄʜᴀɴɴᴇʟ ᴏʀ ᴄʜᴀɴɴᴇʟ ɪᴅ ɪs ᴡʀᴏɴɢ.\n\nᴅᴇᴛᴀɪʟs: {e}</b>")
+        
     file_id = str(post.id)
-    string = 'file_'
-    string += file_id
+    string = f"file_{file_id}"
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    user_id = message.from_user.id
-    user = await get_user(user_id)
+    
     if WEBSITE_URL_MODE == True:
         share_link = f"{WEBSITE_URL}?share={outstr}"
     else:
         share_link = f"https://t.me/{username}?start={outstr}"
-    if user["base_site"] and user["shortener_api"] != None:
-        short_link = await get_short_link(user, share_link)
-        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
-    else:
-        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
         
+    await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
 
 @Client.on_message(filters.command(['link']) & filters.create(allowed))
 async def gen_link_s(bot, message):
@@ -44,23 +39,21 @@ async def gen_link_s(bot, message):
     if not replied:
         return await message.reply('<b>ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ɢᴇᴛ ᴀ sʜᴀʀᴇᴀʙʟᴇ ʟɪɴᴋ.</b>')
     
-    post = await replied.copy(LOG_CHANNEL)
+    try:
+        post = await replied.copy(LOG_CHANNEL)
+    except Exception as e:
+        return await message.reply(f"<b>❌ ᴇʀʀᴏʀ: ʙᴏᴛ ɪs ɴᴏᴛ ᴀᴅᴍɪɴ ɪɴ ʟᴏɢ ᴄʜᴀɴɴᴇʟ.\n\nᴅᴇᴛᴀɪʟs: {e}</b>")
+        
     file_id = str(post.id)
-    string = f"file_"
-    string += file_id
+    string = f"file_{file_id}"
     outstr = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    user_id = message.from_user.id
-    user = await get_user(user_id)
+    
     if WEBSITE_URL_MODE == True:
         share_link = f"{WEBSITE_URL}?share={outstr}"
     else:
         share_link = f"https://t.me/{username}?start={outstr}"
-    if user["base_site"] and user["shortener_api"] != None:
-        short_link = await get_short_link(user, share_link)
-        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
-    else:
-        await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
         
+    await message.reply(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
 
 @Client.on_message(filters.command(['batch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
@@ -100,7 +93,6 @@ async def gen_link_batch(bot, message):
         return await message.reply(f'<b>ᴇʀʀᴏʀs - {e}</b>')
     
     sts = await message.reply("<b>🔺 ɢᴇɴᴇʀᴀᴛɪɴɢ ʟɪɴᴋ...</b>")
-
     FRMT = "<b>ɢᴇɴᴇʀᴀᴛɪɴɢ ʟɪɴᴋ...</b>\n\n<b>ᴛᴏᴛᴀʟ ᴍᴇssᴀɢᴇs:</b> {total}\n<b>ᴅᴏɴᴇ:</b> {current}\n<b>ʀᴇᴍᴀɪɴɪɴɢ:</b> {rem}\n<b>sᴛᴀᴛᴜs:</b> {sts}"
 
     outlist = []
@@ -119,24 +111,25 @@ async def gen_link_batch(bot, message):
             "channel_id": f_chat_id,
             "msg_id": msg.id
         }
-        og_msg +=1
+        og_msg += 1
         outlist.append(file)
 
     with open(f"batchmode_{message.from_user.id}.json", "w+") as out:
         json.dump(outlist, out)
-    post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️ ʙᴀᴛᴄʜ ɢᴇɴᴇʀᴀᴛᴇded ғᴏʀ ғɪʟᴇsᴛᴏʀᴇ.")
+    
+    try:
+        post = await bot.send_document(LOG_CHANNEL, f"batchmode_{message.from_user.id}.json", file_name="Batch.json", caption="⚠️ ʙᴀᴛᴄʜ ɢᴇɴᴇʀᴀᴛᴇᴅ ғᴏʀ ғɪʟᴇsᴛᴏʀᴇ.")
+    except Exception as e:
+        return await sts.edit(f"<b>❌ ᴇʀʀᴏʀ: ʙᴏᴛ ᴄᴀɴɴᴏᴛ sᴇɴᴅ ᴅᴏᴄᴜᴍᴇɴᴛ ᴛᴏ ʟᴏɢ ᴄʜᴀɴɴᴇʟ.\n\nᴅᴇᴛᴀɪʟs: {e}</b>")
+        
     os.remove(f"batchmode_{message.from_user.id}.json")
     string = str(post.id)
     file_id = base64.urlsafe_b64encode(string.encode("ascii")).decode().strip("=")
-    user_id = message.from_user.id
-    user = await get_user(user_id)
+    
     if WEBSITE_URL_MODE == True:
         share_link = f"{WEBSITE_URL}?share=BATCH-{file_id}"
     else:
         share_link = f"https://t.me/{username}?start=BATCH-{file_id}"
-    if user["base_site"] and user["shortener_api"] != None:
-        short_link = await get_short_link(user, share_link)
-        await sts.edit(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\ncontains `{og_msg}` files.\n\n🖇️ sʜᴏʀᴛ ʟɪɴᴋ :- {short_link}</b>")
-    else:
-        await sts.edit(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\ncontains `{og_msg}` files.\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
+        
+    await sts.edit(f"<b>⭕ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ʟɪɴᴋ:\n\nᴄᴏɴᴛᴀɪɴs `{og_msg}` ғɪʟᴇs.\n\n🔗 ᴏʀɪɢɪɴᴀʟ ʟɪɴᴋ :- {share_link}</b>")
     
