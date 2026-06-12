2dc4b8e6651e88a7132d8f793b2f039b1781158b
EpicSenpai <arvindrathod5476@gmail.com>
2026-06-12 11:41:36 +0530

    Update users_api.py

--- plugins/users_api.py
+++ plugins/users_api.py
@@ -1,17 +1,7 @@
-# © Telegram : @KingVJ01 , GitHub : @VJBots
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 import requests
 import json
 from motor.motor_asyncio import AsyncIOMotorClient
 from plugins.clone import mongo_db
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 async def get_short_link(user, link):
     api_key = user["shortener_api"]
@@ -19,12 +9,8 @@ async def get_short_link(user, link):
     print(user)
     response = requests.get(f"https://{base_site}/api?api={api_key}&url={link}")
     data = response.json()
-    if data["status"] == "success" or rget.status_code == 200:
+    if data["status"] == "success" or response.status_code == 200:
         return data["shortenedUrl"]
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
 
 async def get_user(user_id):
     user_id = int(user_id)
@@ -39,16 +25,9 @@ async def get_user(user_id):
         user = mongo_db.user.find_one({"user_id": user_id})
     return user
 
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 async def update_user_info(user_id, value:dict):
     user_id = int(user_id)
     myquery = {"user_id": user_id}
     newvalues = { "$set": value }
     mongo_db.user.update_one(myquery, newvalues)
-
-# Don't Remove Credit Tg - @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+    
